968ee8e54a435b815e7a43bf328df8d64711d964
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-04-12 22:04:35 +0530

    firehose setup app initial commit

--- terraform/app-aps1.tf
+++ terraform/app-aps1.tf
@@ -1,35 +1,35 @@
-module "app-aps1" {
-  source = "./modules/application"
-  providers = {
-    aws = aws.aps1
-  }
-  instance_config = {
-    cidr_block     = var.app_cidr
-    app_name       = var.app_name
-    ami_id         = data.aws_ami.amazon_linux_gp3.id
-    instance_type  = var.instance_type
-    key_name       = var.key_name
-    user_data      = data.template_file.user_data.rendered
-    public_enabled = true
-  }
-}
+# module "app-aps1" {
+#   source = "./modules/application"
+#   providers = {
+#     aws = aws.aps1
+#   }
+#   instance_config = {
+#     cidr_block     = var.app_cidr
+#     app_name       = var.app_name
+#     ami_id         = data.aws_ami.amazon_linux_gp3.id
+#     instance_type  = var.instance_type
+#     key_name       = var.key_name
+#     user_data      = data.template_file.user_data.rendered
+#     public_enabled = true
+#   }
+# }
 
-data "template_file" "user_data" {
-  template = file("${path.module}/scripts/application.sh")
-}
+# data "template_file" "user_data" {
+#   template = file("${path.module}/scripts/application.sh")
+# }
 
-data "aws_ami" "amazon_linux_gp3" {
-  provider    = aws.aps1
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "amazon_linux_gp3" {
+#   provider    = aws.aps1
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
