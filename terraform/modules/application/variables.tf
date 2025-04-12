091d8d4b409073ae54fbc26d8f43c2880b1b38e8
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-04-12 21:37:07 +0530

    firehose setup app initial commit

--- terraform/modules/application/variables.tf
+++ terraform/modules/application/variables.tf
@@ -59,7 +59,7 @@ variable "ingress_rules" {
   default = [
     { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
     { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-    { from_port = 22, to_port = 22, protocol = "ssh", cidr_blocks = ["0.0.0.0/0"] }
+    { from_port = 22, to_port = 22, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
   ]
 }
 
