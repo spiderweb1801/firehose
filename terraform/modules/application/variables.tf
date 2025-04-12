c0aa4add56bb6a64620e92924450b4a23a226615
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-04-12 21:11:11 +0530

    firehose setup app initial commit

--- terraform/modules/application/variables.tf
+++ terraform/modules/application/variables.tf
@@ -4,24 +4,24 @@ locals {
     public_subnets = {
       subnet1 = {
         name              = "public-subnet-1"
-        cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 0)
+        cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, 0)
         availability_zone = "${data.aws_region.current.name}a"
       },
       subnet2 = {
         name              = "public-subnet-2"
-        cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+        cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, 1)
         availability_zone = "${data.aws_region.current.name}b"
       }
     },
     private_subnets = {
       subnet1 = {
         name              = "private-subnet-1"
-        cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+        cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, 2)
         availability_zone = "${data.aws_region.current.name}a"
       },
       subnet2 = {
         name              = "private-subnet-2"
-        cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 3)
+        cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, 3)
         availability_zone = "${data.aws_region.current.name}b"
       }
     }
