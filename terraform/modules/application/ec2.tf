ec79dc7e719b11f303fe063034abe250f8f6bc77
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-04-12 20:16:46 +0530

    firehose setup app initial commit

--- terraform/modules/application/ec2.tf
+++ terraform/modules/application/ec2.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "app_server" {
     Name = var.instance_config.app_name
   }
 
-  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+  vpc_security_group_ids = [aws_security_group.default_sg.id]
 }
 
 resource "aws_security_group" "default_sg" {
