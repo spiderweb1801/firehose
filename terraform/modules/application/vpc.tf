b95d33cbb0d708625db81aacee068a5771f199ac
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-04-13 18:55:38 +0530

    firehose setup

--- terraform/modules/application/vpc.tf
+++ terraform/modules/application/vpc.tf
@@ -44,14 +44,15 @@ resource "aws_subnet" "private_subnets" {
 resource "aws_route_table" "public_rtb" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
   tags = {
     Name = "${var.instance_config.app_name}-public-route-table"
   }
+}
+
+resource "aws_route" "route_to_internet" {
+  route_table_id            = aws_route_table.public_rtb.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "public" {
@@ -76,14 +77,15 @@ resource "aws_nat_gateway" "nat" {
 resource "aws_route_table" "private_rtb" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
   tags = {
     Name = "${var.instance_config.app_name}-private-route-table"
   }
+}
+
+resource "aws_route" "route_to_pub" {
+  route_table_id            = aws_route_table.private_rtb.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.nat.id
 }
 
 resource "aws_route_table_association" "private" {
