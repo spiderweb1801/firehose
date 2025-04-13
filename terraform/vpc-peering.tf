a003e5e7ac0f3ca696f0ab9a49722325d0e0d6ff
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-04-13 13:50:15 +0530

    firehose setup

--- terraform/vpc-peering.tf
+++ terraform/vpc-peering.tf
@@ -9,14 +9,14 @@ resource "aws_vpc_peering_connection" "app_splunk_peering_aps1" {
 }
 
 resource "aws_route" "app_to_splunk" {
-  for_each                  = [module.app-aps1.network_details.public_rtb, module.app-aps1.network_details.private_rtb]
+  for_each                  = { "pub-rtb"=module.app-aps1.network_details.public_rtb, "private-rtb"=module.app-aps1.network_details.private_rtb}
   route_table_id            = each.value
   destination_cidr_block    = var.splunk_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.app_splunk_peering_aps1.id
 }
 
 resource "aws_route" "splunk_to_app" {
-  for_each                  = [module.app-aps1.splunk-aps1.public_rtb, module.splunk-aps1.network_details.private_rtb]
+  for_each                  = [module.splunk-aps1.public_rtb, module.splunk-aps1.network_details.private_rtb]
   route_table_id            = each.value
   destination_cidr_block    = var.app_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.splunk_peering.id
